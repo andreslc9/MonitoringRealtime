cb9888468cf3b82dce47890d3b3d2824586a737b
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2024-09-09 23:32:33 -0500

    ajuste de url

--- realtimeGraph/urls.py
+++ realtimeGraph/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("historical/data", download_csv_data, name="historical-data"),
+    path("city-stats",get_avg_temperature_and_humidity_for_city,name="city-stats")
 ]
